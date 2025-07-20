ef788e95b40e568dd489862cc28b15add478bd88
Eddrick-23 <122147240+Eddrick-23@users.noreply.github.com>
2025-07-20 13:19:35 +0800

    WIP: Frontend components  LeftDrawer, LocationRadio, and ShiftSizeRadio components; integrate them into Session page

---
+++ src/frontend/src/components/LeftDrawer.vue
@@ -0,0 +1,25 @@
+<template>
+    <Drawer 
+        v-model:visible="modelValue" 
+        header="Sidebar" 
+        :modal="false" 
+        :dismissable="false"
+        position="left" 
+        class="!w-64 p-0 custom-drawer">
+        <p>sidebar content</p>
+    </Drawer>
+</template> 
+
+<script setup>
+import Drawer from 'primevue/drawer';
+const modelValue = defineModel();
+
+</script>
+
+<style>
+/* Force the internal drawer panel width to be 16rem (256px) */
+:deep(.p-drawer-left) {
+  width: 16rem !important;
+  max-width: 16rem !important;
+}
+</style>
--- src/frontend/src/pages/Session.vue
+++ src/frontend/src/pages/Session.vue
@@ -3,9 +3,38 @@
     <div v-if="loginFailed" class="w-full h-screen flex flex-col text-center justify-center">
       <p style="color: red">Login failed. Please try again later.</p>
     </div>
-    <div v-else-if="!loading">
-      <p>Session Page</p>
-      <Grid :day="1" :location="'MCC'"/>
+    
+    <div v-else-if="!loading" class="p-4 flex">
+      <LeftDrawer v-model="drawerVisible"/>
+      <div class="relative min-h-screen transition-all duration-300 w-full" :class="{ 'ml-64': drawerVisible }">
+        <div class="absolute bottom-4 left-0 -ml-4">
+          <Button :icon="drawerVisible ? 'pi pi-arrow-left' : 'pi pi-arrow-right'" @click="drawerVisible = !drawerVisible"/>
+        </div>
+        <div class="grid grid-flow-col grid-cols-8 grid-rows-1 gap-2 ml-4 mr-4">
+          <div class="col-span-2 flex flex-col items-center justify-center">
+            <Select v-model="selectedGrid" :options="AllGrids" optionLabel="name" placeholder="Select Grid" optionValue="code" :showClear="true" class="w-full" />
+          </div>
+          <div class="col-span-2 flex flex-col items-center justify-center">
+            <InputText type="text" placeholder="Name" v-model="typedName" class="w-full"/>
+          </div>
+          <div class="col-span-2 flex flex-col items-center justify-center">
+            <ButtonGroup class="flex w-full">
+              <Button icon="pi pi-plus" :disabled="!selectedGrid || !typedName" class="flex-1"/>
+              <Button icon="pi pi-minus" :disabled="!selectedGrid || !typedName" class="flex-1"/>
+            </ButtonGroup>
+          </div>
+          <div class="col-span-1 flex flex-col items-center justify-center">
+            <LocationRadio v-model="selectedLocation"/> 
+          </div>
+          <div class="col-span-1 flex flex-col items-center justify-center"> 
+            <ShiftSizeRadio v-model="selectedShiftSize"/>
+          </div>
+        </div>
+        <Divider align="center" class="control-panel-divider">
+          <b>Control Panel</b>
+        </Divider> 
+        <Grid :day="1" :location="'MCC'"/>
+      </div>
     </div>
     <div v-else class="w-full h-screen flex flex-col text-center justify-center">
       <p>Loading...</p>
@@ -15,15 +44,36 @@
 
 
 <script setup>
-import Grid from '../components/Grid.vue'
-import axios from 'axios'
-import { onMounted,ref } from 'vue'
-import endpoints from '../api/api'
-
+import Grid from '../components/Grid.vue';
+import axios from 'axios';
+import { onMounted,ref } from 'vue';
+import endpoints from '../api/api';
+import Divider from 'primevue/divider';
+import Select from 'primevue/select';
+import InputText from 'primevue/inputtext';
+import ButtonGroup from 'primevue/buttongroup';
+import Button from 'primevue/button';
+import LocationRadio from '../components/LocationRadio.vue';
+import ShiftSizeRadio from '../components/ShiftSizeRadio.vue';
+import LeftDrawer from '../components/LeftDrawer.vue';
 const apiBaseUrl = import.meta.env.VITE_BACKEND_DOMAIN
 
 const loginFailed = ref(false);
 const loading = ref(true);
+const selectedLocation = ref('MCC'); //radio button
+const selectedShiftSize = ref('Full'); // radio button
+const selectedGrid = ref(null); // select UI
+const AllGrids = ref([
+  {name:'DAY1:MCC',code:'DAY1:MCC'},
+  {name:'DAY1:HCC1',code:'DAY1:HCC1'},
+  {name:'DAY1:HCC2',code:'DAY1:HCC2'},
+  {name:'DAY2:MCC',code:'DAY2:MCC'},
+  {name:'DAY2:HCC1',code:'DAY2:HCC1'},
+  {name:'DAY2:HCC2',code:'DAY2:HCC2'},
+  {name:'NIGHT DUTY', code:'DAY3:MCC'},
+])
+const typedName = ref(null); // input box
+const drawerVisible = ref(true); 
 
 onMounted(() => {
   axios.get(apiBaseUrl + endpoints.login, {
@@ -48,3 +98,10 @@ onMounted(() => {
 });
 
 </script>
+
+
+<style>
+.control-panel-divider{
+  --p-divider-border-color:#0a0100 
+}
+</style>
